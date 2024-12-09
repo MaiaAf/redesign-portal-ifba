edbc0d2421675876a45907b04f9c7060aff606a0
MaiaAf <arthurmaia0125@gmail.com>
2024-12-09 13:49:11 -0300

    Colocar scripts locais no final

--- scroll.js
+++ scroll.js
@@ -1,8 +1,8 @@
 // Agradecimentos à Fireship: https://www.youtube.com/watch?v=T33NN_pPeNI
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    // if (entry.isIntersecting) entry.target.classList.add("aparecer"); // Aparecer apenas uma vez
-    entry.target.classList.toggle("aparecer", entry.isIntersecting); // Aparecer toda vez
+    if (entry.isIntersecting) entry.target.classList.add("aparecer"); // Aparecer apenas uma vez
+    // entry.target.classList.toggle("aparecer", entry.isIntersecting); // Aparecer toda vez
   });
 });
 
