e3fd18a2319b778928aeb32a3f735efac76557ca
MaiaAf <arthurmaia0125@gmail.com>
2024-12-09 17:58:57 -0300

    Transferir o rolamento suave para o script scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,4 +1,20 @@
-// Agradecimentos à Fireship: https://www.youtube.com/watch?v=T33NN_pPeNI
+// Redirecionar suave dos links
+const $root = $("html, body");
+
+$('a[href^="#"]').click(function () {
+  $root.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    500,
+  );
+
+  return false;
+});
+
+// Adicionar classe à elementos baseado na posição da rolagem da tela
+
+// Fonte: Fireship - https://www.youtube.com/watch?v=T33NN_pPeNI
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) entry.target.classList.add("aparecer"); // Aparecer apenas uma vez
