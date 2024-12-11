6a56e3e94f2023df58fa682e329d80d528cc87a7
MaiaAf <arthurmaia0125@gmail.com>
2024-12-11 16:44:55 -0300

    Consertar paginação e navegação no carrosel, carrosel inicial

--- scripts/carrosel.js
+++ scripts/carrosel.js
@@ -1,14 +1,41 @@
-var swiper = new Swiper(".swiper-container", {
-    loop: true,
-    mousewheel: {
-        invert: false,
-        interval: 400
-      },
-      spaceBetween: 0,
-      navigation: {
-        prevEl: ".anterior",
-        nextEl: ".proximo"
-      },
-      plugins: [SwiperPluginNavigation]
-    }
-);
+Swiper.use([SwiperPluginPagination, SwiperPluginNavigation]);
+
+// Cada carrosel é uma instância da classe Swiper
+
+const swiperBiblioteca = new Swiper("#swiper-biblioteca", {
+  loop: true,
+  pagination: {
+    el: "#swiper-biblioteca .swiper-plugin-pagination",
+    clickable: true,
+    bulletClass: "swiper-plugin-pagination__item", // Corrected
+    bulletActiveClass: "is-active",
+  },
+  mousewheel: {
+    invert: false,
+    interval: 400,
+  },
+  spaceBetween: 0,
+  navigation: {
+    prevEl: "#swiper-biblioteca .anterior",
+    nextEl: "#swiper-biblioteca .proximo",
+  },
+});
+
+const swiperInicial = new Swiper("#swiper-inicial", {
+  loop: true,
+  pagination: {
+    el: "#swiper-inicial .swiper-plugin-pagination",
+    clickable: true,
+    bulletClass: "swiper-plugin-pagination__item", // Corrected
+    bulletActiveClass: "is-active",
+  },
+  mousewheel: {
+    invert: false,
+    interval: 400,
+  },
+  spaceBetween: 0,
+  navigation: {
+    prevEl: "#swiper-inicial .anterior",
+    nextEl: "#swiper-inicial .proximo",
+  },
+});
